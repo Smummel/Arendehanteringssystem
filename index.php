42b8797b8cf79ded2d8b730087dccc5a676bbfde
Smummel <samuelbasson06@gmail.com>
2025-03-22 16:04:30 +0100

    Design done prett mucg

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hej</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div id="background">
+        <div id="fadebox"></div>
+        <img src="pexels-photo-1655901.jpeg" alt="">
+    </div>
+    <div id="nav">
+        <div id="navbox">
+            <div id="logo">m</div>
+            <a href="">Hem</a>
+            <a href="">Om oss</a>
+            <a href="">Logga in</a>
+        </div>
+    </div>
+    <div id="homecontent">
+        <p>Problem med spöken, förbannelser eller saker som rör sig av sig själva? Vi fixar det!</p>
+        <h1>Mystic Fixers</h1>
+        <div id="homecontentButtons">
+            Välkommen! Hur kan vi hjälpa dig?
+            <a id="akut" href="">Akut hjälp!</a>
+            <a href="">Rapportera ett fall</a>
+            <a href="">Se våra tidigare fall</a>
+        </div>
+    </div>
+    <div id="footer">
+        <h1></h1>
+        <div id="footbar">
+            <div id="footerstuff">
+                <a href="">FAQ</a>
+                <p>&copy;Mystic Fixers 2025</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
