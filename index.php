35326022d6f9e8c5daeba9711a886f25a71e74d2
Smummel <samuelbasson06@gmail.com>
2025-03-22 19:43:29 +0100

    fix

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             <div id="logo">m</div>
             <a href="index.php?location=home">Hem</a>
             <a href="index.php?location=omoss">Om oss</a>
-            <a href="">Logga in</a>
+            <a href="index.php?location=login">Logga in</a>
         </div>
     </div>
     <?php
@@ -36,20 +36,23 @@
                     <div id="homecontentButtons">
                         Välkommen! Hur kan vi hjälpa dig?
                         <a id="akut" href="tel:666">Akut hjälp!</a>
-                        <a href="">Rapportera ett fall</a>
-                        <a href="">Se våra tidigare fall</a>
+                        <a href="index.php?location=report">Rapportera ett fall</a>
+                        <a href="index.php?location=blog">Se våra tidigare fall</a>
                     </div>
                 </div>
             <?php
         } elseif($location=='omoss'){
             ?>
             <div id="omosscontent">
-                
+                <h1>Om oss</h1>
+                <p>Välkommen till Mystic Fixers - experterna på att lösa det övernaturliga! Vi är ett team av erfarna utredare, mediet och teknikspecialister som hanterar allt från envisa spöken och mystiska skuggor till förbannade föremål och okända energier. Oavsett om du upplever märkliga ljud, plötsliga temperaturförändringar eller andra oförklarliga fenomen, står vi redo att hjälpa dig. Med en kombination av vetenskapliga metoder och gammal visdom ser vi till att din omgivning blir trygg igen. Mystic Fixers - för paranormala problem utan naturliga lösningar.</p>
             </div>
             <?php
         } elseif($location=='loggain'){
             ?>
-            
+            <div id="login">
+                
+            </div>
             <?php
         } elseif($location=='faq'){
             ?>
@@ -57,9 +60,10 @@
                 <h2>Vanliga frågor:</h2>
                 <p>Q: Hur vet jag om jag behöver er hjälp?<br>A: Rapportera ditt fall så överväger vi. </p>
                 <p>Q: Jag tror att jag är hemsökt, vad ska jag göra? <br>A: Rapportera ditt fall till oss, ring vårt nödnummer om det behövs.</p>
-                <p>Q: Jag har en känsla av att någon tittar på mig när jag är ensam, men ser ingen. Är det något övernaturligt?<br>A: Ring nödnumret. </p>
+                <p>Q: Min spegelbild försöker prata med mig, vad borde jag göra?<br>A: Ring nödnummret omedelbart. </p>
                 <p>Q: Är det sant att spöken gillar att spela på Ouija-brädor?<br>A: Nej, det är bara påhitt. Spöken spelar biljard.</p>
-                <p>Q: Finns verkligen spöken?<br>A: Kolla på våra inlägg och avgör själv.</p>
+                <p>Q: Finns verkligen spöken?<br>A: Ja.</p>
+                <p>Q: Jag hittade något slemmigt i min sons sovrum, är det spökslemm?<br>A: Vi är redan påväg.</p>
             </div>
             <?php
         }
